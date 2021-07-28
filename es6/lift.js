c43ffcd915af884089bd119044023013f8861c5b
Sushil Chaurasiya <sushil.chaurasiya@gmail.com>
2021-07-28 09:36:48 +0530

    lift_problem

---
+++ es6/lift.js
@@ -0,0 +1,14 @@
+var data;
+function getData(params) {
+    data = params;
+    //document.getElementById('status').innerText = data;
+    // document.getElementsByClassName('status').innerText = data;
+    // document.getElementsByTagName('span').textContent = data;
+    var totalspan = document.getElementsByTagName('span');
+    for (i=0; i<totalspan.length; i++){
+        totalspan[i].textContent = data;
+    }
+
+
+    
+}
